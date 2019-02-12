c543c9a6a2a2d7315814ed0600666b5424354d13
beau-martin <e.martin6@students.clark.edu>
2019-02-11 20:53:35 -0800

    Project 2 Submission

--- page1.php
+++ page1.php
@@ -11,7 +11,64 @@
 </head>
 <body>
 
+<?php include("includes/header.html"); ?>
 
+<p>There are 7 different controls on this page which will allow you to change the way page 2 looks. Use them to configure page 2.</p>
+
+<form action="page2.php" method="get">
+
+    <label for="name">Name:
+        <input type="text" id="name" name="name" required>
+    </label>
+    <br><br>
+
+    Are you:<br>
+
+    <label for="male"><input id="male" type="radio" name="gender" value="man" required> Male</label><br>
+    <label for="female"><input id="female" type="radio" name="gender" value="woman"> Female</label><br><br>
+
+    <p>Which of these social media sites do you use? </p>
+    <label for="facebook">
+        <input type="checkbox" id="facebook" name="facebook" value="true"> Facebook
+    </label><br>
+    <label for="linkedin">
+        <input type="checkbox" id="linkedin" name="linkedin" value="true"> LinkedIn
+    </label><br>
+    <label for="slack">
+        <input type="checkbox" id="slack" name="slack" value="true"> Slack
+    </label>
+    <br><br>
+
+<p>Which would you prefer as your means of transportation:</p>
+
+<select name="transport">
+    <option value="bicycle">Bicycle</option>
+    <option value="helijet">Helijet</option>
+    <option value="hyperloop">Hyperloop</option>
+    <option value="supercar">Supercar</option>
+    <option value="tank">Tank</option>
+</select><br><br>
+
+<!-- <p>Which color header would you like:</p>
+<select name="header_color">
+    <option value="red">Red</option>
+    <option value="blue">Blue</option>
+    <option value="black">Black</option>
+<select><br><br>
+
+<p>Which color footer would you like:</p>
+<select name="footer_color">
+    <option value="red">Red</option>
+    <option value="blue">Blue</option>
+    <option value="black">Black</option>
+<select><br><br> -->
+
+<input type="submit" value="submit"><br><br>
+
+</form>
+
+
+<?php include("includes/footer.html"); ?>
 
 <!-- jQuery -->
 <script src="js/jquery-3.3.1.min.js"></script>
--- page2.php
+++ page2.php
@@ -11,7 +11,54 @@
 </head>
 <body>
 
+<?php include("includes/header.html"); ?>
 
+<p>Hello, <?php echo $_GET["name"];?>! <?php echo "You're the " . $_GET["gender"];?>!!</p>
+
+<?php
+
+if(!isset($_GET['facebook']) && !isset($_GET['linkedin']) && !isset($_GET['slack'])){
+    echo "<p>You didn't use any of the social media sites listed.<p>";
+} else {echo "<p>Here are the social media sites you use:</p>";}
+
+if(isset($_GET["facebook"])){
+    echo '<img src="images/facebook.png" alt="Facebook" height="128" width="128">';
+} 
+ 
+if (isset($_GET["linkedin"])) {
+    echo '<img src="images/linkedin.png" alt="LinkedIn" height="128" width="128">';
+} 
+
+if(isset($_GET["slack"])) {
+    echo '<img src="images/slack.png" alt="Slack" height="128" width="128">';
+}
+
+?>
+<br><br>
+<p>Here is what you would most like as your means of transportation:</p>
+
+<?php 
+
+switch ($_GET["transport"]) {
+    case "bicycle":
+        echo '<img src="images/bicycle.png" alt="bicycle" height="176" width="300">';
+        break;
+    case "helijet":
+        echo '<img src="images/helijet.png" alt="helijet" height="122" width="300">';
+        break;
+    case "hyperloop":
+        echo '<img src="images/hyperloop.jpg" alt="hyperloop" height="168" width="300">';
+        break;
+    case "supercar":
+        echo '<img src="images/supercar.png" alt="supercar" height="116" width="300">';
+        break;
+    case "tank":
+        echo '<img src="images/tank.png" alt="tank" height="134" width="300">';
+        break;
+}
+        ?>
+<br><br>
+<?php include("includes/footer.html"); ?>
 
 <!-- jQuery -->
 <script src="js/jquery-3.3.1.min.js"></script>
